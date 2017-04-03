80ea2ea33f4230957797aa9633057cedd55ed86d
tatianaanthony <tatiana.anthony@students.olin.edu>
2017-04-03 14:19:38 -0400

    works for FiveNoteScale

--- read_MIDIs.py
+++ read_MIDIs.py
@@ -19,31 +19,33 @@ def read_midi(filename):
         for j in range(len(track)):
             msg = track[j]
             print(msg)
-            print(msg.type)
+            # print(msg.type)
             is_new_note = True
             for old_note in open_notes:
+                print(is_new_note)
                 old_note.duration += msg.time
-                
-                if old_note.note == msg.note:
-                    is_new_note = False
-                    if msg.type == 'note_on':
-                        if msg.note.velocity == 0:
+                if msg.type == 'note_on' or msg.type == 'note_off':
+                    if old_note.value == msg.note:
+                        is_new_note = False
+                        if msg.type == 'note_on':
+                            if msg.velocity == 0:
+                                list_of_notes.append(old_note)
+                                open_notes.remove(old_note)
+                        elif msg.type == 'note_off':
                             list_of_notes.append(old_note)
                             open_notes.remove(old_note)
-                    elif msg.type == 'note_off':
-                        list_of_notes.append(old_note)
-                        open_notes.remove(old_note)
-            if is_new_note:
-                new_note = Note(msg.note, msg.velocity)
-                open_notes.append(new_note)
 
-                
-            try:
-                nextmsg = track[j+1]
-                print(nextmsg.time)
-            except:
-                pass
+            if is_new_note:
+                if len(old_notes) >0:
+                        new_note = Note(msg.note, msg.velocity)
+                        open_notes.append(new_note);
+            # try:
+            #     nextmsg = track[j+1]
+            #     # print(nextmsg.time)
+            # except:
+            #     pass
     return list_of_notes
 
 if __name__ == "__main__":
-    read_midi('Untitled_441406.mid')
+    a = read_midi('Untitled_441406.mid')
+    print(a)
