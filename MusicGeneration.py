2d0bec25c656fe0a63aa6d5000d155c832abb5a9
tatianaanthony <tatiana.anthony@students.olin.edu>
2017-04-03 14:35:17 -0400

    adding read_midi to MusicGeneration

--- MusicGeneration.py
+++ MusicGeneration.py
@@ -2,9 +2,54 @@
 from tkinter import *
 from tkinter import messagebox
 from tkinter import font
+import mido
 
+class Note:
+    def __init__(self,tone = 60, volume = 60, duration = 0):
+        self.tone = tone
+        self.duration = duration
+        self.volume = volume
 
+def read_midi(filename):
+    mid = mido.MidiFile(filename)
+    print(mid)
+    list_of_notes = []
+    open_notes = []
+    for i, track in enumerate(mid.tracks):
+        print('Track {}: {}'.format(i, track.name))
+        # test_track = track[350]
+        # print(track[350])
+        for j in range(len(track)):
+            msg = track[j]
+            print(msg)
+            # print(msg.type)
+            is_new_note = True
+            may_be_note = False
+            if msg.type == 'note_on' or msg.type == 'note_off':
+                may_be_note = True
+            for old_note in open_notes:
+                print(is_new_note)
+                old_note.duration += msg.time
+                if may_be_note:
+                    if old_note.tone == msg.note:
+                        is_new_note = False
+                        if msg.type == 'note_on':
+                            if msg.velocity == 0:
+                                list_of_notes.append(old_note)
+                                open_notes.remove(old_note)
+                        elif msg.type == 'note_off':
+                            list_of_notes.append(old_note)
+                            open_notes.remove(old_note)
 
+            if is_new_note and may_be_note:
+                new_note = Note(msg.note, msg.velocity)
+                open_notes.append(new_note);
+            # try:
+            #     nextmsg = track[j+1]
+            #     # print(nextmsg.time)
+            # except:
+            #     pass
+    return list_of_notes
 
 def MIDI_clean(filename):
 	"""
